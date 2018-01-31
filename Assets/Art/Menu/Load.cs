cc6375ad28e0da2710c7edbff8ca43dddfb7733b
Vlad <petechukvlad@gmail.com>
2018-02-01 00:18:30 +0200

    Menu+ progress bar

---
+++ Assets/Art/Menu/Load.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour {
+
+    public int sceneID;
+    public Image loadImg;
+    public Text progressText;
+	
+	void Start ()
+    {
+        StartCoroutine(SLoad());
+	}
+
+    IEnumerator SLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            loadImg.fillAmount = progress;
+            progressText.text = string.Format("{0:0.0}%", progress * 100);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Art/Menu/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	public void StartGame()
+    {
+        
+       // SceneManager.LoadScene(1);
+    }
+    public void LoadGame()
+    {
+
+    }
+    public void Setings()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}

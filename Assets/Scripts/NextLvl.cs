ed21f043a73f993409381bd388970e4bce139aed
Vlad <petechukvlad@gmail.com>
2018-02-08 01:24:45 +0200

    bug fixing + next lvl + respawn(begin)

---
+++ Assets/Scripts/NextLvl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextLvl : MonoBehaviour {
+
+    public int nextlvl;
+   
+   void OnTriggerEnter(Collider col)
+    {
+      
+        if (col.tag == "Player")
+        {
+            SceneManager.LoadScene(nextlvl);
+        }
+    }
+
+}
